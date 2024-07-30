016205baac29816ce85bdeec3b6eee6e52dfe1ff
Walid Nasim <wanasim92@gmail.com>
2024-07-29 22:28:48 -0400

    using wasm module suffix for vercel runtime support

--- packages/env/src/tokenizers/node.ts
+++ packages/env/src/tokenizers/node.ts
@@ -1,14 +1,24 @@
 // Note: This is using th WASM implementation of tiktoken which is 60x faster
+import cl100k_base from "tiktoken/encoders/cl100k_base";
+import { init, Tiktoken } from "tiktoken/lite/init";
+import wasm from "tiktoken/lite/tiktoken_bg.wasm?module";
 import type { Tokenizer } from "./types.js";
 import { Tokenizers } from "./types.js";
 
-import { get_encoding } from "tiktoken";
+declare const WebAssembly: any; // Add this line to declare the WebAssembly object
 
+(async function () {
+  await init((imports) => WebAssembly.instantiate(wasm, imports));
+})();
 class TokenizerSingleton {
   private defaultTokenizer: Tokenizer;
 
   constructor() {
-    const encoding = get_encoding("cl100k_base");
+    const encoding = new Tiktoken(
+      cl100k_base.bpe_ranks,
+      cl100k_base.special_tokens,
+      cl100k_base.pat_str,
+    );
 
     this.defaultTokenizer = {
       encode: (text: string) => {
